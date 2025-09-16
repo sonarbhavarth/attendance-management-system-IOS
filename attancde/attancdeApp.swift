cec5bf0436dca9c2d77fa5db2d1e50c5fc3775f1
bhavarth sonar <sonarbhavarth@gmail.com>
2025-09-16 20:57:14 +0530

    Initial Commit

---
+++ attancde/attancdeApp.swift
@@ -0,0 +1,17 @@
+//
+//  attancdeApp.swift
+//  attancde
+//
+//  Created by bhavarth on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct attancdeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ attancdeTests/attancdeTests.swift
@@ -0,0 +1,16 @@
+//
+//  attancdeTests.swift
+//  attancdeTests
+//
+//  Created by bhavarth on 16/09/25.
+//
+
+import Testing
+
+struct attancdeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
